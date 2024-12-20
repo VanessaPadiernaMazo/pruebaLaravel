deb6af2be4fdac34c4494e3130cd3cb68eb7dba7
VanessaPadiernaMazo <vanepadierna@gmail.com>
2024-12-20 16:17:51 -0500

    terminar la pruebaLaravel, crear la api rest y hacer ensayos

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,12 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductoController;
-use App\Http\Controllers\Api\CategoriaController;
 
 Route::get('/productos', [ProductoController::class, 'index']);
-Route::get('/categorias', [CategoriaController::class, 'index']);
+Route::get('/productos/{id}', [ProductoController::class, 'show']);
+Route::post('/crearproductos', [ProductoController::class, 'store']);
+Route::put('/editarproductos/{id}', [ProductoController::class, 'update']);
+Route::delete('/eliminarproductos/{id}', [ProductoController::class, 'destroy']);
 
 //Route::apiResource('productos', ProductoController::class);
 //Route::get('/productos', function (){
